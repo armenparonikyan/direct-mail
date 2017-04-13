9c092ba8601ab83a3137fd7cf2d30b31b9d2dd44
Armen Paronikyan <armparonikyan@gmail.com>
2017-04-13 20:38:41 +0400

    Fixed buttons

--- script.js
+++ script.js
@@ -13,11 +13,15 @@ $(window).resize(()=>{
 		$('.offers_small').removeClass('invisible');
 		$('#main-text').removeClass('main-text').addClass('small-text');
 		$('.effort_text').removeClass('effort_text').addClass('effort_text_sm');
+		$('.big_button1').css('font-size','13px');
+		$('.pink_button').css('font-size','13px');
 
 	}else{
 		$('.offers_small').addClass('invisible');
 		$('.offers').removeClass('invisible');
 		$('#main-text').addClass('main-text').removeClass('small-text');
 		$('.effort_text_sm').addClass('effort_text').removeClass('effort_text_sm');
+		$('.big_button1').css('font-size','28px');
+		$('.pink_button').css('font-size','29px');
 	}
 });
