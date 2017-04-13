2744d43002d52847e0afcd0b6edd63fe81dda07d
Armen Paronikyan <armparonikyan@gmail.com>
2017-04-13 20:30:39 +0400

    Fixed colors ad sizes

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ $(window).resize(()=>{
 		$(".links").removeClass('invisible');
 		$(".menu-icon").addClass('invisible');
 	}
-	if(w<=500){
-		$('#main-text').removeClass('main-text').addClass('small-text');
-	}else{
-		$('#main-text').addClass('main-text').removeClass('small-text');
-	}
 	if(w<=700){
 		$('.offers').addClass('invisible');
 		$('.offers_small').removeClass('invisible');
+		$('#main-text').removeClass('main-text').addClass('small-text');
+		$('.effort_text').removeClass('effort_text').addClass('effort_text_sm');
+
 	}else{
 		$('.offers_small').addClass('invisible');
-		$('.offers').removeClass('invisible')
+		$('.offers').removeClass('invisible');
+		$('#main-text').addClass('main-text').removeClass('small-text');
+		$('.effort_text_sm').addClass('effort_text').removeClass('effort_text_sm');
 	}
 });
